b3df96edc0f5170e085d5b73b515786226d7d744
Petr <Petruxamix@mail.ru>
2022-10-01 02:15:48 +0300

    SearchInputFocus

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+import { useRef } from 'react';
 import { useContext } from 'react';
 import { SearchContext } from '../../App';
 import styles from './search.module.scss';
 
 const Search = () => {
   const { searchValue, setSearchValue } = useContext(SearchContext);
+  const inputRef = useRef(); // обращение к элементу
+
+  const onClickClear = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {
+    console.log(document.querySelector('input'));
+  }, []);
+
   return (
     <div className={styles.root}>
       <svg
@@ -40,6 +53,7 @@ const Search = () => {
         />
       </svg>
       <input
+        ref={inputRef}
         placeholder="Поиск пиццы..."
         className={styles.input}
         onChange={(e) => setSearchValue(e.target.value)}
@@ -48,7 +62,7 @@ const Search = () => {
       {searchValue && (
         <svg
           className={styles.clearIcon}
-          onClick={() => setSearchValue('')}
+          onClick={onClickClear}
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg">
           <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
