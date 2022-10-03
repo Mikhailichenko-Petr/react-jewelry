aa367e81d93602ce4ae3f8390628d7fae75e2ae8
Petr <Petruxamix@mail.ru>
2022-10-04 00:04:33 +0300

    UseNavigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 import Sort from '../components/Sort';
 import Categories from '../components/Categories';
@@ -9,14 +10,15 @@ import { Skeleton } from '../components/jewelryBlock/skeleton';
 import Pagination from '../components/pagination/pegination';
 import { setCategory, setPage } from '../redux/slices/filterSlice';
 import { SearchContext } from '../App';
+import QueryString from 'qs';
 
 export const Home = () => {
-  const { category, page } = useSelector((state) => state.filterSlice);
+  const navigate = useNavigate();
+  const { category, page, sort } = useSelector((state) => state.filterSlice);
   const dispatch = useDispatch();
 
   const [item, setItem] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [sortActive, setSortActive] = useState({ name: 'популярности', type: 'rating' });
   const { searchValue } = useContext(SearchContext); // CONTEXT
 
   const indexCategory = (id) => {
@@ -32,7 +34,7 @@ export const Home = () => {
     axios
       .get(
         `https://632e4bcbf9b533cc58ee4523.mockapi.io/items/?page=${page}&limit=8&sortBy=${
-          sortActive.type
+          sort.type
         }&order='asc'${searchValue ? `?&search=${searchValue}` : ''}&${
           category > 0 ? `category=${category}` : ''
         }`,
@@ -42,7 +44,16 @@ export const Home = () => {
         setLoading(false);
       });
     window.scrollTo(0, 0); //скролит наверх при рендеренге
-  }, [category, sortActive, page]);
+  }, [category, sort, page]);
+
+  useEffect(() => {
+    const quertyString = QueryString.stringify({
+      sortProperty: sort.type,
+      category,
+      page,
+    });
+    navigate(`?${quertyString}`);
+  }, [category, sort, page]);
 
   return (
     <div className="container">
